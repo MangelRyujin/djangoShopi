7a56e570925667ed2153069d1ee7d31858c2a2d6
mangelryujin <mangelryujin@gmail.com>
2024-07-01 09:36:21 -0400

    feat[general]: create general app and add general to settings

--- djangoShopi/settings.py
+++ djangoShopi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.accounts',
+    'apps.general',
     # allauth authentications
     'allauth',
     'allauth.account',
